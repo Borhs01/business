167838588ecc2ff100aba794f0fe804ca2d306bb
Borhs01 <155558801+Borhs01@users.noreply.github.com>
2025-06-12 15:49:04 +0100

    latest update

---
+++ index.js
@@ -0,0 +1,6 @@
+    window.onclick = function(event) {
+      const modal = document.getElementById('formModal');
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    }
